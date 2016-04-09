da73830d1285068a466faa617651ee4571b56d85
Mateusz Pietraszewski <pietraszewski.mateusz@gmail.com>
2016-04-09 22:00:34 +0100

    Boid

--- Games Engines 2 Assignment/Assets/Scripts/Boid.cs
+++ Games Engines 2 Assignment/Assets/Scripts/Boid.cs
@@ -3,13 +3,59 @@ using System.Collections;
 
 public class Boid : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass = 1.0f;
+    public float damping;
+
+    public float maxSpeed;
+    public float maxForce;
+
+    Vector3 Seek(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        toTarget.Normalize();
+        Vector3 desired = toTarget * maxSpeed;
+        return desired - velocity;
+    }
+
+    public Vector3 Arrive(Vector3 targetPos)
+    {
+        Vector3 toTarget = targetPos - transform.position;
+
+        float slowingDistance = 15.0f;
+        float distance = toTarget.magnitude;
+        if (distance < 0.5f)
+        {
+            velocity = Vector3.zero;
+            return Vector3.zero;
+        }
+        float ramped = maxSpeed * (distance / slowingDistance);
+
+        float clamped = Mathf.Min(ramped, maxSpeed);
+        Vector3 desired = clamped * (toTarget / distance);
+
+        return desired - velocity;
+    }
+
+    public Vector3 Flee(Vector3 targetPos, float range)
+    {
+        Vector3 desiredVelocity;
+        desiredVelocity = transform.position - targetPos;
+        if (desiredVelocity.magnitude > range)
+        {
+            return Vector3.zero;
+        }
+        desiredVelocity.Normalize();
+        desiredVelocity *= maxSpeed;
+
+        return desiredVelocity - velocity;
+    }
+
+    void Update()
+    {
+
+    }
+
 }
